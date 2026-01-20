1661c98b1ba1d3895da0428300f1de9d3b8afbc5
DaeYoung0726 <pdy000726@naver.com>
2026-01-20 23:19:22 +0900

    feat: JPA Auditing 기반 공통 BaseEntity 추가

---
+++ src/main/kotlin/prac/blog/domain/common/BaseEntity.kt
@@ -0,0 +1,24 @@
+package prac.blog.domain.common
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@EntityListeners(AuditingEntityListener::class)
+@MappedSuperclass
+abstract class BaseEntity {
+    // non-null로 설정. 하지만, 영속화 시점에 값이 채워지기에 lateinit
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    lateinit var createdAt: LocalDateTime
+        protected set
+
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    lateinit var modifiedAt: LocalDateTime
+        protected set
+}
