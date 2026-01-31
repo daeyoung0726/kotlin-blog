cbd8fa8555e87eb47cb0222a18c53c3ea7dfbaed
DaeYoung0726 <pdy000726@naver.com>
2026-01-31 17:24:23 +0900

    feat: 댓글 좋아요 카운트 증가 쿼리 작성 * 동시 요청에 대해 원자 연산으로 처리

--- src/main/kotlin/prac/blog/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/prac/blog/domain/comment/repository/CommentRepository.kt
@@ -1,7 +1,20 @@
 package prac.blog.domain.comment.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import prac.blog.domain.comment.entity.Comment
 
 interface CommentRepository : JpaRepository<Comment, Long>, CustomCommentRepository {
+    @Modifying
+    @Query(
+        value = """
+            UPDATE comment
+            SET like_count = like_count + CASE WHEN :likeStatus THEN 1 ELSE -1 END
+            WHERE id = :commentId
+                AND (like_count > 0 OR :likeStatus = true)
+        """,
+        nativeQuery = true
+    )
+    fun updateLikeCount(commentId: Long, likeStatus: Boolean)
 }
