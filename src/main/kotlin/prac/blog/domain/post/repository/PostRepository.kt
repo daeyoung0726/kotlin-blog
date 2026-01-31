a6eef705c361249786ef658ebdd8ed1caa2fc942
DaeYoung0726 <pdy000726@naver.com>
2026-01-31 17:20:44 +0900

    feat: 게시글 좋아요 카운트 증가 쿼리 작성 * 동시 요청에 대해 원자 연산으로 처리

--- src/main/kotlin/prac/blog/domain/post/repository/PostRepository.kt
+++ src/main/kotlin/prac/blog/domain/post/repository/PostRepository.kt
@@ -1,7 +1,21 @@
 package prac.blog.domain.post.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import prac.blog.domain.post.entity.Post
 
 interface PostRepository : JpaRepository<Post, Long>, CustomPostRepository {
+
+    @Modifying
+    @Query(
+        value = """
+            UPDATE post
+            SET like_count = like_count + CASE WHEN :likeStatus THEN 1 ELSE -1 END
+            WHERE id = :postId
+                AND (like_count > 0 OR :likeStatus = true)
+        """,
+        nativeQuery = true
+    )
+    fun updateLikeCount(postId: Long, likeStatus: Boolean)
 }
