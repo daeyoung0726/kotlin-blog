7787be1c4ca0925668f866c6be65c5b06090848b
DaeYoung0726 <pdy000726@naver.com>
2026-02-03 02:39:19 +0900

    refact: 불필요 어노테이션 삭제

--- src/main/kotlin/prac/blog/security/authentication/CustomUserDetails.kt
+++ src/main/kotlin/prac/blog/security/authentication/CustomUserDetails.kt
@@ -1,11 +1,9 @@
 package prac.blog.security.authentication
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import prac.blog.domain.user.entity.User
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class CustomUserDetails(
     val userId: Long,
     private val username: String,
